818c1f7908a6a3c973ff4001f017c16ee018008b
Ed Charbeneau <ed.charbeneau@gmail.com>
2012-10-21 20:27:54 -0400

    VS 2012 New MVC4 Project
    File > New Project
    Visual C# > ASP.NET MVC 4 Web Application
    Basic Template

---
+++ Foundation3SinglePageRWD/Foundation3SinglePageRWD/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Foundation3SinglePageRWD
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
